903282acddf8754f3edcaf61f5a3b71509d87521
Brian Guo <brianguosh@gmail.com>
2014-10-08 13:21:13 -0400

    "updated"

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import re
 from bs4 import BeautifulSoup
+import google
 ## Reads in text files
 f = open('text.txt')
 text = f.read()
@@ -8,6 +9,7 @@ f.close()
 names = re.findall( '[A-Z][a-z]*\s[A-Z][a-z]*', text, flags=0)
 ## Creates dictionary with name as value and frequency in text as key
 names_dict = {}
+r = google.search("who played spiderman", tld = 'com', lang = 'en', num = 10, start = 0, stop = 10, pause = 0.0)
 for name in names:
     if name in names_dict:
         names_dict[name] += 1
