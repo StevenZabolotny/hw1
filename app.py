4ebb94a42e12e0602151671438a284d683699891
Fish Milnikiewicz <fmilniki@gmail.com>
2014-10-09 17:17:29 -0400

    worked on interface/form

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import search
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
-def index():
-    if request.method=="GET":
-        return render_template("index.html");
+@app.route("/home")
+def home():
+    button = request.args.get("b",None)
+    question = request.args.get("q",None)
+    if request.method=="GET" or button==None or (button!=None and question==None):
+        return render_template("home.html");
     else:
         b = request.form['b'] #Submit button
         q = request.form['q'] #Type of question
@@ -15,7 +18,11 @@ def index():
             info = search.search('q','s'); #This gives the other file the query it needs to google and the type of question it is so it can return what we're looking for.
             return render_template("search.html",info = info); #Takes you to page to return search results
         else:
-            return render_template("index.html");
+            return render_template("home.html");
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
 
 if __name__=="__main__":
     app.debug = True
